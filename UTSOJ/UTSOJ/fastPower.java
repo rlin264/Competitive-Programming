b876e9de98279421144f4df2d2cacf004a7ad8a1
Raymond Lin <rayster264@gmail.com>
2018-12-04 10:31:14 -0500

    first

---
+++ UTSOJ/UTSOJ/fastPower.java
@@ -0,0 +1,15 @@
+package UTSOJ;
+
+import java.io.*;
+
+public class fastPower {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+		String[] tokens = stdin.readLine().split(" ");
+		int N = Integer.parseInt(tokens[0]);
+		int M = Integer.parseInt(tokens[1]);
+	}
+
+}
